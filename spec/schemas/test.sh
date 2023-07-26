0585317d05ece38cd10341b7fd1643c583344ae2
Thore Husfeldt <thore.husfeldt@gmail.com>
2023-07-26 11:25:09 +0200

    Update tests

--- spec/schemas/test.sh
+++ spec/schemas/test.sh
@@ -2,17 +2,11 @@ echo "Valid problem.yaml"
 echo "=================="
 echo 
 
-for f in ../../examples/problem_settings/valid/*/problem.yaml; do
+for f in ../../examples/problem_settings/*/*/problem.yaml; do
 	echo $f
 	cue vet problem_package.cue "$f" -d "#problem_settings"
 	echo $? 
+	cue vet problem_package.cue "$f" -d "#problem_settings_icpc"
+	echo $? 
 	echo
-done
-
-echo "Invalid problem.yaml"
-echo "===================="
-echo 
-for f in ../../examples/problem_settings/invalid/*/problem.yaml; do
-	echo $f
-	cue vet problem_package.cue "$f" -d "#problem_settings"
 done
