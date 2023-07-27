9191a4d2f967c0700bc020cc6ef4b5881f5bd481
Thore Husfeldt <thore.husfeldt@gmail.com>
2023-07-27 11:35:28 +0200

    Examples of invalid YAML, also test that these are rejected

--- spec/schemas/test.sh
+++ spec/schemas/test.sh
@@ -2,17 +2,54 @@ cue vet ../../examples/problem_full.yaml problem_package.cue
 if [ $? -eq 0 ]; then
 	echo "OK: Full examples accepted"
 else
-	echo "WARNING: Not all full examples accepted"
+	echo "WARNING: Not all full examples validated"
 fi
 cue vet ../../examples/problem_icpc.yaml problem_package.cue  -d "#icpc"
 if [ $? -eq 0 ]; then
 	echo "OK: ICPC examples accepted"
 else
-	echo "WARNING: Not all ICPC examples accepted"
+	echo "WARNING: Not all ICPC examples validated against #icpc"
 fi
-for f in ../../examples/problem_settings/invalid/*/problem.yaml; do
+
+output_dir="tmp"
+mkdir -p  "$output_dir/full"
+
+# Check that "full" problems in examples/problem_full.yaml
+# are *rejected* by the #icpc subset (so either something
+# is wrong or the example should be moved to examples/problem_icpc.yaml)
+
+counter=0
+awk -v RS='---' -v output_dir="$output_dir" '
+	{
+		file_counter++;
+		output_file = output_dir "/full/problem_" file_counter ".yaml"
+		print $0 > output_file;
+
+	}' ../../examples/problem_full.yaml
+
+for f in $output_dir/full/*.yaml; do
+ 	cue vet problem_package.cue -d "#icpc" "$f" >/dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		echo "WARNING: $f not rejected by #icpc"
+	fi
+done
+
+# Check that invalid problems in examples/problem_invalid.yaml
+# are indeed *rejected* 
+
+counter=0
+mkdir -p  "$output_dir/invalid"
+awk -v RS='---' -v output_dir="$output_dir" '
+	{
+		file_counter++;
+		output_file = output_dir "/invalid/problem_" file_counter ".yaml"
+		print $0 > output_file;
+
+	}' ../../examples/problem_invalid.yaml
+
+for f in $output_dir/invalid/*.yaml; do
  	cue vet problem_package.cue "$f" >/dev/null 2>&1
 	if [ $? -eq 0 ]; then
-		echo "WARNING: $f"
+		echo "WARNING: $f not rejected"
 	fi
 done
