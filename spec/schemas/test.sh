99010daed4445251d563abd5933421b3e5ebee7a
Thore Husfeldt <thore.husfeldt@gmail.com>
2023-07-27 10:55:02 +0200

    test against both schemas, test invalid yaml individually

--- spec/schemas/test.sh
+++ spec/schemas/test.sh
@@ -1,2 +1,18 @@
-cue vet ../../examples/problem_full.yaml problem_package.cue  -d "#problem_settings"
-cue vet ../../examples/problem_icpc.yaml problem_package.cue  -d "#problem_settings_icpc"
+cue vet ../../examples/problem_full.yaml problem_package.cue
+if [ $? -eq 0 ]; then
+	echo "OK: Full examples accepted"
+else
+	echo "WARNING: Not all full examples accepted"
+fi
+cue vet ../../examples/problem_icpc.yaml problem_package.cue  -d "#icpc"
+if [ $? -eq 0 ]; then
+	echo "OK: ICPC examples accepted"
+else
+	echo "WARNING: Not all ICPC examples accepted"
+fi
+for f in ../../examples/problem_settings/invalid/*/problem.yaml; do
+ 	cue vet problem_package.cue "$f" >/dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		echo "WARNING: $f"
+	fi
+done
