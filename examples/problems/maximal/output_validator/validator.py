c7a6e6e60cdf109a7256788667b91b8344934593
JoelNiemela <joel@niemela.se>
2024-08-16 17:19:08 +0200

    Improve output_validator

--- examples/problems/maximal/output_validator/validator.py
+++ examples/problems/maximal/output_validator/validator.py
@@ -1,9 +1,16 @@
 import sys
 
 [_, in_file, ans_file, feedback_dir] = sys.argv
-output = sys.stdin.read()
+output: str = sys.stdin.read()
 
-if output.isdigit() and int(output) == 42:
-	sys.exit(42) # Accepted
-else:
+indata: str
+with open(in_file) as f:
+	indata = f.read()
+
+try:
+	if abs(int(output) - int(indata)) == {{example_constant}}:
+		sys.exit(42) # Accepted
+	else:
+		sys.exit(43) # Rejected
+except ValueError:
 	sys.exit(43) # Rejected
