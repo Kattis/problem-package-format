1a9926555d8d7a3b1f0bc1dadaa546945bc1f246
Maarten Sijm <9739541+mpsijm@users.noreply.github.com>
2025-04-20 23:38:30 +0200

    examples/problems/maximal: Add examples of visualizers

---
+++ examples/problems/maximal/test_case_visualizer/input_visualizer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys
+from xml.sax.saxutils import escape
+
+input_file = open(sys.argv[1]).read().strip()
+answer_file = open(sys.argv[2]).read().strip()
+args = sys.argv[3:]
+with open("testcase.svg", "w") as f:
+    print(f"<svg><text>args: {escape(' '.join(args))}</text></svg>", file=f)
